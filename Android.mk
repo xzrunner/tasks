119bc9fdc6e6e6bf527322bc94c27270b5f7863c
yangkaibo <yangkaiboo@163.com>
2017-03-11 17:33:30 +0800

    android build

--- Android.mk
+++ Android.mk
@@ -7,6 +7,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := tasks
 LOCAL_CFLAGS := -std=gnu99
 
+LOCAL_C_INCLUDES := \
+	${LOGGER_SRC_PATH} \
+
 LOCAL_SRC_FILES := \
 	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.c" -print)) \
 
